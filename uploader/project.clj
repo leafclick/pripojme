b380b834068791ab605083098bcf23e12908617e
Petr Effenberger <petr@effy.cz>
2016-11-02 10:52:20 +0100

    haystack uploader

---
+++ uploader/project.clj
@@ -0,0 +1,10 @@
+(defproject uploader "0.1.0-SNAPSHOT"
+  :description "pripoj.me skyspark data uploader"
+  :url "http://pripoj.me"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [clj-http "3.1.0"]
+                 [cheshire "5.6.3"]
+                 [org.projecthaystack/haystack-java "3.0.0"]
+                 [org.clojure/data.csv "0.1.3"]])
